1afe305e458297f9a3adce042750a7202f219bbf
Sweetmantech <sweetmantech@gmail.com>
2018-01-03 19:29:58 -0500

    initial welcome message for all users, initial firebse realtime database storage

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import * as firebase from 'firebase';
+
 import { GiftedChat } from 'react-native-gifted-chat';
 
 import { Actions } from 'react-native-router-flux';
@@ -7,8 +9,41 @@ import { Actions } from 'react-native-router-flux';
 class Conversations extends React.Component {
   state = {
     conversations: [],
-    placeholder: "New conversation with..."
+    placeholder: "New conversation with...",
+    uid: this.props.uid
   };
+
+  componentWillMount() {
+    this.getFirebaseConversations();
+  }
+
+  getFirebaseConversations() {
+    var conversations = [];
+    var conversationsRef = firebase.database().ref('users/' + this.state.uid + '/conversations');
+    conversationsRef.on('value', function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+          // key is name of each conversation
+          var key = childSnapshot.key;
+          // childData will be the actual contents of the child
+          var childData = childSnapshot.val();
+          this.setState(prevState => {
+            conversations = prevState.conversations;
+            conversations.push({
+              _id: conversations.length + 1,
+              text: key,
+              user: {
+                _id: 2
+              }
+            });
+            return {
+              conversations: conversations,
+              placeholder: prevState.placeholder,
+              uid: prevState.uid,
+            }
+          });
+      }.bind(this));
+    }.bind(this));
+  }
 
   render() {
     return(
